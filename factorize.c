67484b51f6c1bc1630c97db89cce9eae440d08a2
FatimaZahraOukmi <Fatimazahra.oukmi@gmail.com>
2023-06-27 15:47:32 -0700

    r

--- factorize.c
+++ factorize.c
@@ -1,29 +1,29 @@
-#include "factor.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * factorize - The function factorize a number
+ * factorize - The function factorizes a number
  * @buffer: pointer to the address of the number
  *
- * Author: Thaoban Abdrasheed
+ * Author: Fz Oukmi
  * Return: int
  */
 int factorize(char *buffer)
 {
+    uint32_t num;
+    uint32_t i;
 
-	u_int32_t num;
-	u_int32_t i;
+    num = atoi(buffer);
 
-	num = atoi(buffer);
+    for (i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            printf("%d=%d*%d\n", num, num / i, i);
+            break;
+        }
+    }
 
-
-	for (i = 2; i < num; i++)
-	{
-		if (num % i == 0)
-		{
-			printf("%d=%d*%d\n", num, num / i, i);
-			break;
-		}
-	}
-
-return (0);
+    return 0;
 }
